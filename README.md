98be23923fd64c36317c06e7ade502e1167622b4
Paulo Sergio da Silva Paixo (LNG-LEI) <pdasilvapaixo@UNIVENTIO.COM>
2017-03-22 12:20:40 +0100

    Update gulp

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Layout Manager for Google Chrome.
 You can install the extension from:
 
 https://chrome.google.com/webstore/detail/lexisnexis-layout-manager/ijpiffheldgmdkbaohoilabdnmeinand
+
+# Dependencies
+
+To compile install `node` and `gulp`.
+
+Dependencies:
+- gulp
+- gulp-clean
+- gulp-htmlmin
+- gulp-minify
+- gulp-zip 
+
+To build the solution use `gulp`
+
+To package use `gulp package` to create the `chrome-layout-manager-extension.zip`
--- gulpfile.js
+++ gulpfile.js
@@ -2,63 +2,65 @@ var gulp   = require('gulp');
 var clean = require('gulp-clean');
 var htmlmin = require('gulp-htmlmin');
 var minify = require('gulp-minify');
+var zip = require('gulp-zip');
 
 gulp.task('compress', function() {
     gulp.src(['js/layout.js'])
         .pipe(minify({
             ext:{
-                //src:'-src.js',
+                src:'-src.js',
                 min:'.js'
             },
             exclude: ['tasks'],
             ignoreFiles: ['.combo.js', '-min.js']
         }))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('build/js'))
 });
 
-gulp.task('compress2', function() {
+gulp.task('compress-background', function() {
     gulp.src(['background.js'])
         .pipe(minify({
             ext:{
-                //src:'-src.js',
+                src:'-src.js',
                 min:'.js'
             },
             exclude: ['tasks'],
             ignoreFiles: ['.combo.js', '-min.js']
         }))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('build'))
 });
 
 gulp.task('copy', function () {
     gulp.src(['manifest.json'])
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('copy-img', function () {
     gulp.src('img/*')
-        .pipe(gulp.dest('dist/img/'));
+        .pipe(gulp.dest('build/img/'));
 });
 
 gulp.task('copy-css', function () {
     gulp.src('css/*')
-        .pipe(gulp.dest('dist/css/'));
-});
-
-
-gulp.task('watch', function () {
-    console.log("Watch here!");
-    //gulp.watch('src/*.coffee', ['scripts']);
+        .pipe(gulp.dest('build/css/'));
 });
 
 gulp.task('minify', function() {
     return gulp.src(['popup.html'])
         .pipe(htmlmin({collapseWhitespace: true,removeComments: true,}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('clean', function () {
-    return gulp.src(['dist/*.*','dist/js/*.*'], {read: false})
+    return gulp.src(['build/**/*'], {read: false})
         .pipe(clean());
 });
 
-gulp.task('default', ['compress','compress2', 'minify','copy','copy-css','copy-img']);
+gulp.task('package', function() {
+    gulp.src('build/**/*')
+        .pipe(zip('chrome-layout-manager-extension.zip'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['clean','compress','compress-background', 'minify','copy','copy-css','copy-img']);
+
